80d0a3db38a0b51a8548b43ce98e3d8114fc2184
Mishiiii <43751802+Mishiiii@users.noreply.github.com>
2019-03-20 09:50:34 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".button").click(function() {
+    var newTeam = $(".name").val();
+    
+    var comment = $(".thing").val();
+    
+    var answer = $(".times").val();
+    var total = answer * 52 * 31;
+    
+    var full = "In the year 2050," + " " + newTeam + " " + "will eat " + " " + comment + " " + total + ". ";
+    $(".results").text(full);
+  
+});  
+    // var firstNAme = "Alex";
+    // var lastName = "Barganier";
+    
+    // var fullName = "<p class=\"nameResult\">" + firstNAme + " " + lastName + "</p>";
+    // $(".results").append(fullName);
+    
+    
